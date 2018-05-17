09165b2d6631a98207075eabf37a2360da214f60
Cameron Dimitroff <hawtrods@gmail.com>
2018-05-16 18:01:48 -0700

    giphy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var GphApiClient = require('giphy-js-sdk-core')
+client = GphApiClient("trilogy")
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -12,7 +14,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 3001;
 
 // Initialize Express
 var app = express();
@@ -34,25 +36,24 @@ mongoose.connect("mongodb://localhost/week18Populater");
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://giphy.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("a").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
       result.link = $(this)
-        .children("a")
         .attr("href");
+      result.source = $(this)
+        .children("img")
+        .attr("src");
 
       // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
+      db.div.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
